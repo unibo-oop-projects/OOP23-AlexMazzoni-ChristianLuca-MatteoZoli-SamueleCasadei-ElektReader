728a7fd2fc5734684ba803f12a73f4f8dfc1ab63
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-02-04 16:14:50 +0100

    edited SongController class

--- app/src/main/java/elektreader/controller/SongsController.java
+++ app/src/main/java/elektreader/controller/SongsController.java
@@ -1,11 +1,9 @@
 package elektreader.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+import java.util.List;
+import elektreader.api.PlayList;
 import elektreader.api.Song;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,41 +13,45 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
 public class SongsController {
+    
     private List<VBox> btnSongs;
+    private final TilePane songPane;
+    private final double CONTAINER_W = 100, CONTAINER_H = 125, BTN_W = 90, BTN_H = 80,
+        IMGFIT_W = 75, IMGFIT_H = 85, DEF_SPACING = 5;
 
-    public SongsController(final TilePane songsList) {
-        songsList.setPadding(new Insets(10));
+    public SongsController(final TilePane songPane, PlayList selected) {
+        this.songPane = songPane;
+        this.btnSongs = selected.getSongs().stream()
+            .map(s -> createButton(s))
+            .toList();
 
-        this.btnSongs = new ArrayList<>(Collections.emptyList());
+        // GUIController.getReader().getCurrentPlaylist().get().getSongs().stream()
+        //     .forEach(s -> this.btnSongs.add(createButton(s)));
 
-        GUIController.getReader().getCurrentPlaylist().get().getSongs().stream()
-            .forEach(s -> this.btnSongs.add(createButton(s)));
-
-        songsList.getChildren().addAll(btnSongs);
+        this.songPane.getChildren().addAll(btnSongs);
     }
     
     private VBox createButton(final Song song) {
         VBox container = new VBox();
-        container.setPrefSize(100, 125);
-        container.setStyle("-fx-alignment: center;");
-        container.setStyle("-fx-background-color: d3d3d3;");
         Button btn = new Button();
-        btn.setAlignment(Pos.CENTER);
-        btn.setPrefSize(90, 80);
-        btn.setStyle("-fx-background-color: transparent;");
-        btn.setTextAlignment(TextAlignment.JUSTIFY); 
         ImageView img = new ImageView(ClassLoader.getSystemResource("icons/Dark/Media/AudioWave.png").toString());
-        img.setFitHeight(75.0);
-        img.setFitWidth(85.0);
-        img.setPreserveRatio(true);
-        btn.setGraphic(img);
         Label duration = new Label(song.DurationStringRep());
         Label title = new Label(song.getName());
+        container.setSpacing(DEF_SPACING);
+        container.setPrefSize(CONTAINER_W, CONTAINER_H);
+        container.setStyle("-fx-alignment: center;");
+        container.setStyle("-fx-background-color: d3d3d3;");
+        btn.setAlignment(Pos.CENTER);
+        btn.setPrefSize(BTN_W, BTN_H);
+        btn.setStyle("-fx-background-color: transparent;");
+        btn.setTextAlignment(TextAlignment.JUSTIFY);
+        img.setFitHeight(IMGFIT_W);
+        img.setFitWidth(IMGFIT_H);
+        img.setPreserveRatio(true);
+        btn.setGraphic(img);
         container.getChildren().addAll(btn, duration, title);
         return container;
     }
-
-
 
     public List<VBox> getBtnSongs() {
         return this.btnSongs;
