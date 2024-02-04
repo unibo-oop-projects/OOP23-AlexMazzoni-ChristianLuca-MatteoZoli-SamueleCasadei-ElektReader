e52f9772bbe347c9c7bcebac3c0c6523b4b1a746
Christian Luca <Giovanni.luca@studio.unibo.it>
2024-02-04 18:41:28 +0100

    Started thinking about API and implementation of MediaControl. Work in progress

--- app/src/main/java/elektreader/api/MediaControl.java
+++ app/src/main/java/elektreader/api/MediaControl.java
@@ -1,5 +1,40 @@
 package elektreader.api;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.media.Media;
+import javafx.util.Duration;
+
 public interface MediaControl {
+
+    //private boolean isOver(final Song song);
+
+    public void play();
+
+    public void pause();
+
+    public void stop();
+
+    public boolean next();
+
+    public boolean prev();
+    
+    public boolean loop(final Song song);
+
+    public void setQueue(final Iterable<Song> queue);
+
+    public void setSong(final Media song);
+
+    public boolean setRepSpeed();
+
+    public boolean setProgress(final Duration duration);
+
+    public Duration getDuration();
+
+    public boolean setVolume(final DoubleProperty volume);
+
+    public DoubleProperty getVolume();
+
+    public void mute();
+
 
 }
--- app/src/main/java/elektreader/model/Mp3MediaControl.java
+++ app/src/main/java/elektreader/model/Mp3MediaControl.java
@@ -1,7 +1,97 @@
 package elektreader.model;
 
 import elektreader.api.MediaControl;
+import elektreader.api.Song;
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 public class Mp3MediaControl implements MediaControl{
+
+    private MediaPlayer mediaPlayer;
+    private Iterable<Song> queue;
+
+
+    public Mp3MediaControl(Iterable<Song> queue, final Media song) {
+        this.queue = queue;
+        mediaPlayer = new MediaPlayer(song);
+    }
+
+    @Override
+    public void play() {
+        this.mediaPlayer.play();     //At this moment, mediaPlayer status will be set to status.PLAYING
+    }
+
+    @Override
+    public void pause() {
+        this.mediaPlayer.pause();   //At this moment, mediaPlayer status will be set to status.PAUSED
+    }
+
+    @Override
+    public void stop() {
+        this.mediaPlayer.stop();
+    }
+
+    @Override
+    public boolean next() {
+        
+    }
+
+    @Override
+    public boolean prev() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'prev'");
+    }
+
+    @Override
+    public boolean loop(Song song) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loop'");
+    }
+
+    @Override
+    public void setQueue(Iterable<Song> queue) {
+       this.queue = queue;
+    }
+
+    @Override
+    public void setSong(Media song) {
+        this.mediaPlayer = new MediaPlayer(song);
+    }
+
+    @Override
+    public boolean setRepSpeed() {
+        /* TODO */
+    }
+
+    @Override
+    public boolean setProgress(Duration duration) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setProgress'");
+    }
+
+    @Override
+    public Duration getDuration() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDuration'");
+    }
+
+    @Override
+    public boolean setVolume(DoubleProperty volume) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVolume'");
+    }
+
+    @Override
+    public DoubleProperty getVolume() {
+        return this.mediaPlayer.
+    }
+
+    @Override
+    public void mute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mute'");
+    }
     
 }
