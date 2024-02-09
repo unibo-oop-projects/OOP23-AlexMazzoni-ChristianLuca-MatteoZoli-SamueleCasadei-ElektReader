82dbdd1c6299568db1dfaa893057c028743f91fd
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-02-09 13:50:32 +0100

    fixing responsiveness of the components

--- app/src/main/java/elektreader/controller/SongsController.java
+++ app/src/main/java/elektreader/controller/SongsController.java
@@ -4,6 +4,7 @@ package elektreader.controller;
 import java.util.List;
 import elektreader.api.PlayList;
 import elektreader.api.Song;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -11,13 +12,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 
+/* a controller for songs graphics, link graphics and logics */
 public class SongsController {
     
     private List<VBox> btnSongs;
     private final FlowPane songPane;
     private final ScrollPane pane;
     private final double CONTAINER_W = 120, CONTAINER_H = 140, BTN_W = 90, BTN_H = 80,
-        IMGFIT_W = 75, IMGFIT_H = 85, DEF_SPACING = 15;
+        IMGFIT_W = 75, IMGFIT_H = 85, DEF_SPACING = 2;
 
     public SongsController(final FlowPane songContainer, final ScrollPane pane) {
         this.songPane = songContainer;
@@ -31,10 +33,22 @@ public class SongsController {
         ImageView img = new ImageView(ClassLoader.getSystemResource("icons/Light/Media/AudioWave.png").toString());
         Label duration = new Label(song.DurationStringRep());
         Label title = new Label(song.getName());
-        System.out.println(song.getName());
+
         container.setSpacing(DEF_SPACING);
         container.setPrefSize(CONTAINER_W, CONTAINER_H);
         container.getStyleClass().add("songcontainer");
+        VBox.setMargin(container, new Insets(10));
+        container.setPadding(new Insets(2));
+        container.setOnMouseClicked( event -> {
+            this.btnSongs.stream()
+                .forEach(button -> button.getStyleClass().removeIf(style -> style.equals("selected")));
+            
+            var button = (Button)event.getSource();
+            button.getStyleClass().add("selected");
+            var player = GUIController.getReader().getPlayer();
+            player.setSong(song);
+            player.play();
+        });
 
         btn.setPrefSize(BTN_W, BTN_H);
         btn.getStyleClass().add("songbtn");
