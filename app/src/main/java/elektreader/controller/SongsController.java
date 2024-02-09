9d0debff624c0b1c5728666125c368a0cf7861c2
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-02-09 11:20:22 +0100

    new graphics for songs and playlists (flowpane instead of tilepane)

--- app/src/main/java/elektreader/controller/SongsController.java
+++ app/src/main/java/elektreader/controller/SongsController.java
@@ -4,23 +4,24 @@ package elektreader.controller;
 import java.util.List;
 import elektreader.api.PlayList;
 import elektreader.api.Song;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 
 public class SongsController {
     
     private List<VBox> btnSongs;
-    private final TilePane songPane;
+    private final FlowPane songPane;
+    private final ScrollPane pane;
     private final double CONTAINER_W = 100, CONTAINER_H = 125, BTN_W = 90, BTN_H = 80,
         IMGFIT_W = 75, IMGFIT_H = 85, DEF_SPACING = 5;
 
-    public SongsController(TilePane songContainer) {
+    public SongsController(final FlowPane songContainer, final ScrollPane pane) {
         this.songPane = songContainer;
+        this.pane = pane;
     }
     
     private VBox createButton(final Song song) {
@@ -34,24 +35,18 @@ public class SongsController {
         container.setMinSize(CONTAINER_W, CONTAINER_H);
         container.getStyleClass().add("songcontainer");
 
-        btn.setAlignment(Pos.CENTER);
         btn.setPrefSize(BTN_W, BTN_H);
-        btn.setStyle("-fx-background-color: transparent;");
-        btn.setTextAlignment(TextAlignment.JUSTIFY);
-        btn.setOnMouseClicked(event -> {
-            var player = GUIController.getReader().getPlayer();
-            if(player.isPresent()){
-                player.get().setSong(song);
-            }
-        });
+        btn.getStyleClass().add("songbtn");
 
         img.setFitHeight(IMGFIT_W);
         img.setFitWidth(IMGFIT_H);
         img.setPreserveRatio(true);
         btn.setGraphic(img);
 
+        duration.getStyleClass().add("songduration");
+        title.getStyleClass().add("songtitle");
+
         container.getChildren().addAll(btn, duration, title);
-        container.setVisible(true);
         return container;
     }
 
@@ -61,5 +56,9 @@ public class SongsController {
             .map(s -> createButton(s))
             .toList();
         songPane.getChildren().addAll(btnSongs);
+    }
+
+    public void responsive(){
+        songPane.setPrefSize(pane.getWidth(), pane.getHeight());
     }
 }
