a3b26516beae00bc8bf0576181436a3b1e1c9580
Christian Luca <Giovanni.luca@studio.unibo.it>
2024-02-12 12:25:09 +0100

    Started filling gridPane with components. Still work in progress.

--- app/src/main/java/elektreader/controller/MediaControlsController.java
+++ app/src/main/java/elektreader/controller/MediaControlsController.java
@@ -31,16 +31,16 @@ public class MediaControlsController {
     MediaControl mediaControl;
 
     public MediaControlsController(final HBox mediaControlPanel, final Slider progressBar) {
-        this.gridPane = new GridPane();
+        this.gridPane = new GridPane(10, 10);
         this.mediaCPanel = mediaControlPanel;
         this.mediaCPanel.getChildren().add(gridPane);
 
         this.gridPane.setPrefHeight(mediaControlPanel.getHeight());
         this.gridPane.setPrefWidth(mediaControlPanel.getWidth());
 
-        this.play_pause = new Button();
-        this.prev_Song = new Button();
-        this.next_Song = new Button();
+        this.play_pause = new Button("Avvio/Pausa");
+        this.prev_Song = new Button("Prev Song");
+        this.next_Song = new Button("Next Song");
         this.current_meta_Song = new Label();
         this.next_meta_Song = new Label();
         this.current_Volume = new Label();
@@ -53,7 +53,13 @@ public class MediaControlsController {
     //This method will be used to insert components into MediaControlPanel. 
     private void insert_in_Panel() {
         this.gridPane.getChildren().clear();
-        this.gridPane.getChildren().addAll(current_meta_Song, prev_Song, play_pause, next_Song, next_meta_Song, current_Volume);
+        //this.gridPane.getChildren().addAll(current_meta_Song, prev_Song, play_pause, next_Song, next_meta_Song, current_Volume);
+        this.gridPane.add(current_meta_Song, 3, 2);
+        this.gridPane.add(prev_Song, 50, 2);
+        this.gridPane.add(play_pause, 55, 2);
+        this.gridPane.add(next_Song, 60, 2);
+        this.gridPane.add(next_meta_Song, 80, 2);
+        this.gridPane.add(current_Volume, (gridPane.getColumnCount() - 3), 2);
         this.gridPane.setVisible(true);
     }
 
@@ -71,10 +77,11 @@ public class MediaControlsController {
         this.prev_Song.setOnMouseClicked(event -> this.mediaControl.prevSong());
         this.prev_Song.setGraphic(current_Volume);
         this.next_Song.setOnMouseClicked(event -> this.mediaControl.nextSong());
+        this.next_Song.setGraphic(current_Volume);
         this.current_meta_Song.setText(this.mediaControl.getCurrentSong().getName() + 
-            (this.mediaControl.getCurrentSong().getArtist().isPresent() ? 
+            "\n" + (this.mediaControl.getCurrentSong().getArtist().isPresent() ? 
             this.mediaControl.getCurrentSong().getArtist().get() : "No artist found"));
-        this.next_meta_Song.setText(this.mediaControl.getNextSong().getName() +
+        this.next_meta_Song.setText("Next song: " + this.mediaControl.getNextSong().getName() +
             (this.mediaControl.getNextSong().getArtist().isPresent() ?
             this.mediaControl.getNextSong().getArtist().get() : "No artist found"));
         this.current_Volume.setText(Double.toString(this.mediaControl.getVolume()));
